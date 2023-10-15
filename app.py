ba82abe485764499cb0b47b699e2144d57778501
Sam-Banankhu <sambanankhumhango@gmail.com>
2023-10-15 02:26:30 +0200

    fixed server bugs deploying for test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from app import app, serve
 
-if __name__ == '__main__':
-    app.run(debug=True, port=9900)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=9900)
 
-# if __name__ == "__main__":
-#     serve(app, host="0.0.0.0", port=8085)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8085)
